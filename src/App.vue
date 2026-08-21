25b0976da792e173960e3415fd494794022f8bfc
gukastan <kmskojb@gmail.com>
2026-08-21 17:28:55 +0900

    refactor: frame weather pet as device screen

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ const route = useRoute()
 </script>
 
 <template>
-  <header class="site-header tamagotchi-header">
-    <RouterLink to="/" class="brand"><span class="brand-mark">🐶</span><span><b>CLOUDIE</b><small>TRAVEL TAMAGOTCHI</small></span></RouterLink>
+  <header class="site-header device-header">
+    <RouterLink to="/" class="brand"><span class="brand-mark">🐶</span><span><b>CLOUDIE</b><small>TRAVEL WEATHER PET</small></span></RouterLink>
     <el-menu :default-active="route.path" mode="horizontal" router class="main-menu" aria-label="주 메뉴">
       <el-menu-item index="/">사용자 모드</el-menu-item>
       <el-menu-item index="/about">서비스 소개</el-menu-item>
@@ -16,6 +16,6 @@ const route = useRoute()
     </el-menu>
     <UnitToggler />
   </header>
-  <main class="app-main tamagotchi-main"><RouterView /></main>
-  <footer class="site-footer tamagotchi-footer"><span>♥ CLOUDIE TRAVEL TAMAGOTCHI</span><span>날씨로 배우는 Vue 3 · Element Plus UI</span></footer>
+  <main class="app-main device-main"><RouterView /></main>
+  <footer class="site-footer device-footer"><span>♥ CLOUDIE TRAVEL WEATHER PET</span><span>날씨로 배우는 Vue 3 · Element Plus UI</span></footer>
 </template>
